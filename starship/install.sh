1955529c0858876e6dcc5eb274a69e35ca34c05e
Thomas KEMKEMIAN <thomas.kemkemian@amadeus.com>
2026-03-17 18:24:31 +0100

    Add installation script for Starship and enhance zsh plugin installation logic

---
+++ starship/install.sh
@@ -0,0 +1,7 @@
+if command -v brew >/dev/null 2>&1; then
+    brew install starship
+elif command -v cargo >/dev/null 2>&1; then
+    cargo install starship
+else
+    echo "Error: Neither Homebrew nor Cargo is available. Please install one of them to proceed."
+fi
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,10 @@
+# Check whether a string exists in a file.
+# Usage: string_in_file "needle" /path/to/file
+string_in_file() {
+	grep -Fq -- "$1" "$2"
+}
+
+
 if [ ! -d "${HOME}/.local/zsh/plugins" ]; then
     mkdir -p ${HOME}/.local/zsh/plugins
 fi
@@ -8,4 +15,16 @@ fi
 
 if [ ! -d "${HOME}/.local/zsh/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.local/zsh/plugins/zsh-syntax-highlighting
+fi
+
+if string_in_file "zsh-autosuggestions.zsh" "${HOME}/.zshrc"; then
+	echo "zsh-autosuggestions already sourced in .zshrc"
+else
+	echo "source ${HOME}/.local/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> "${HOME}/.zshrc"
+fi
+
+if string_in_file "zsh-syntax-highlighting.zsh" "${HOME}/.zshrc"; then
+	echo "zsh-syntax-highlighting already sourced in .zshrc"
+else
+	echo "source ${HOME}/.local/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "${HOME}/.zshrc"
 fi
